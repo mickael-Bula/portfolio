8e1fa2d8b121f23c39518b6366b9bff60ede78a8
Locullus <bula.mickael@gmail.com>
2022-04-28 23:02:20 +0200

    rewrite todolist.js as a module

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,64 +1,60 @@
-// Create a "close" button and append it to each list item
-const myNodelist = document.getElementsByTagName("LI");
-for (const element of myNodelist) {
-  const span = document.createElement("SPAN");
-  const txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  element.appendChild(span);
-}
-
-document.querySelector(".addBtn").addEventListener("click", newElement);
-document.getElementById("form").addEventListener("submit", handleSubmit);
-
-// Click on a close button to hide the current list item
-const close = document.getElementsByClassName("close");
-for (let item of close) {
-  item.addEventListener("click", function () {
-    this.parentElement.style.display = "none";
-  });
-}
-
-// Add a "checked" symbol when clicking on a list item
-const list = document.querySelector("ul");
-list.addEventListener(
-  "click",
-  (e) => {
-    if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked");
+const app = {
+  init: function () {
+    // Create a "close" button and append it to each list item
+    const myNodelist = document.getElementsByTagName("LI");
+    for (const element of myNodelist) {
+      app.addCloseBtn(element);
     }
+
+    document.querySelector(".addBtn").addEventListener("click", app.newElement);
+    document
+      .getElementById("form")
+      .addEventListener("submit", app.handleSubmit);
+
+    // Add a "checked" symbol when clicking on a list item
+    const list = document.querySelector("ul");
+    list.addEventListener(
+      "click",
+      (e) => {
+        if (e.target.tagName === "LI") {
+          e.target.classList.toggle("checked");
+        }
+      },
+      false
+    );
   },
-  false
-);
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  const li = document.createElement("li");
-  const inputValue = document.getElementById("myInput").value;
-  const content = document.createTextNode(inputValue);
-  li.appendChild(content);
-  if (inputValue === "") {
-    alert("Veuillez saisir une tâche");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  const span = document.createElement("SPAN");
-  const txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (let item of close) {
-    item.addEventListener("click", function () {
+  addCloseBtn: function (element) {
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    element.appendChild(span);
+    span.addEventListener("click", function () {
       this.parentElement.style.display = "none";
     });
-  }
-}
+  },
 
-// add a new task when ENTER is pressed
-function handleSubmit(event) {
-  event.preventDefault();
-  newElement();
-}
+  // Create a new list item when clicking on the "Add" button
+  newElement: function () {
+    const li = document.createElement("LI");
+    const inputValue = document.getElementById("myInput").value;
+    const content = document.createTextNode(inputValue);
+    li.appendChild(content);
+    if (inputValue === "") {
+      alert("Veuillez saisir une tâche");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+    app.addCloseBtn(li);
+  },
+
+  // add a new task when ENTER is pressed
+  handleSubmit: function (event) {
+    event.preventDefault();
+    app.newElement();
+  },
+};
+
+document.addEventListener("DOMContentLoaded", app.init);
