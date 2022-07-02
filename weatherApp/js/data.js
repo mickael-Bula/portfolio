d99699fd3f4ac680c147ac3797d98ebbcd82f6b6
Locullus <bula.mickael@gmail.com>
2022-07-02 19:57:01 +0200

    suppression du dossier weatherApp qui possède désormais sa propre page Github-pages

--- weatherApp/js/data.js
+++
@@ -1,8 +0,0 @@
-const weatherIcons = {
-    "Rain": "wi wi-day-rain",
-    "Clouds": "wi wi-day-cloudy",
-    "Clear": "wi wi-day-sunny",
-    "Snow": "wi wi-day-snow",
-    "mist": "wi wi-day-fog",
-    "Drizzle": "wi wi-day-sleet"
-}
