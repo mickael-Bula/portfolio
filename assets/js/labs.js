e6bcabf59b313f9ad35e573db77b04fabe190a32
Locullus <bula.mickael@gmail.com>
2022-08-02 13:38:45 +0200

    ajout dans le labs de l'animation des divs apparaissant successivement

---
+++ assets/js/labs.js
@@ -0,0 +1,54 @@
+const app = {
+  init: function () {
+    console.log("app.init() landing");
+    app.run();
+  },
+
+  removeElement: function () {
+    Array.from(document.getElementsByClassName("slide")).forEach((el) =>
+      el.remove()
+    );
+  },
+
+  // création d'un composant div
+  createElement: function (tag, parent, attributes, i) {
+    const element = document.createElement(tag);
+    for (const attribute in attributes) {
+      // on ajoute à l'élément toutes les propriétés de l'objet attributes passé en paramètre
+      element[attribute] = attributes[attribute];
+      element.textContent = "elément " + i;
+    }
+    parent.appendChild(element);
+    return element;
+  },
+
+  run: function () {
+    window.setInterval(() => {
+      app.removeElement();
+      app.showDIvs();
+    }, 5000);
+  },
+
+  showDIvs: function () {
+    // le parent est soit div.root, soit son dernier enfant
+    const parent =
+      document.getElementById("root").lastElementChild !== null
+        ? document.getElementById("root").lastElementChild
+        : document.getElementById("root");
+
+    for (let i = 1; i < 6; i++) {
+      // les boîtes apparaissent successivement, semblant tomber les unes après les autres
+      window.setTimeout(() => {
+        app.createElement("div", parent, { className: "slide" }, i);
+      }, 300 * i);
+    }
+  },
+};
+
+document.addEventListener("DOMContentLoaded", app.init);
+
+/*
+  Il faut lier ce script au scroll : quand une limite est atteinte, on lance la méthode showDivs.
+ Sinon, si les divs sont affichées on efface.
+ Il faut aussi recentrer les divs
+*/
